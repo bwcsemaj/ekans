7606a0b9f987cbeee88fd2f8500bea71b6575bbb
bwcsemaj <bwc_semaj@live.com>
2020-11-30 02:17:54 -0500

    added ability for snake to grow and move

--- src/main/java/com/backwardscollection/ekans/viewmodel/SnakeFX.java
+++ src/main/java/com/backwardscollection/ekans/viewmodel/SnakeFX.java
@@ -3,6 +3,7 @@ package com.backwardscollection.ekans.viewmodel;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.scene.paint.Color;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.experimental.Accessors;
@@ -21,5 +22,21 @@ public class SnakeFX {
      * Nullary Constructor
      */
     public SnakeFX() {
+    }
+    
+    public void grow(int x, int y) {
+        if (bodyPartsProperty.size() == 0) {
+            bodyPartsProperty.add(new SnakeBodyPartFX(bodyPartsProperty.size(), Color.ALICEBLUE, x, y));
+        } else {
+            bodyPartsProperty.add(new SnakeBodyPartFX(bodyPartsProperty.size(),
+                    Color.ALICEBLUE, x, y, bodyPartsProperty.get(bodyPartsProperty.size() - 1)));
+        }
+       move(x, y);
+    }
+    
+    public void move(int x, int y) {
+        var headBodyPartFX = bodyPartsProperty.get(0);
+        headBodyPartFX.xProperty().set(x);
+        headBodyPartFX.yProperty().set(y);
     }
 }
