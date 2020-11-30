99ee6c862b9e7478c2181716a8632177cfd7e869
bwcsemaj <bwc_semaj@live.com>
2020-11-30 01:13:36 -0500

    initialied food

---
+++ src/main/java/com/backwardscollection/ekans/model/FoodFX.java
@@ -0,0 +1,21 @@
+package com.backwardscollection.ekans.model;
+
+import javafx.beans.property.*;
+import javafx.scene.paint.Color;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true)
+@Value
+@EqualsAndHashCode()
+public class FoodFX {
+    
+    //Attributes
+    public static final Color DEFAULT_FOOD_COLOR = Color.GREEN;
+    private final IntegerProperty xProperty = new SimpleIntegerProperty();
+    private final IntegerProperty yProperty = new SimpleIntegerProperty();
+    private final ObjectProperty<Color> colorProperty = new SimpleObjectProperty<>(DEFAULT_FOOD_COLOR);
+    private final BooleanProperty visibleProperty = new SimpleBooleanProperty(true);
+    
+}
